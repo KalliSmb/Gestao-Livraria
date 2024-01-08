43417550e4175ec833bede2c7dc0bb970413b651
Kayro <kayromatheus584@gmail.com>
2024-01-08 23:03:09 +0000

    Adicionado Login.cs

---
+++ Login.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace LP1_Livraria
+{
+    public class Login
+    {
+        public static bool VerificarLogin(string utilizador, string password)
+        {
+            string caminhoArquivo = "..\\..\\DadosUtilizadores.txt";
+
+            try
+            {
+                if (!File.Exists(caminhoArquivo))
+                {
+                    return false;
+                }
+
+                string[] linhas = File.ReadAllLines(caminhoArquivo);
+
+                foreach (string linha in linhas)
+                {
+                    string[] dados = linha.Split(',');
+                    string[] campos = new string[3];
+
+                    if (dados.Length == dados.Length)
+                    {
+                        Array.Copy(dados, campos, campos.Length);
+
+                        if (campos[0] == utilizador && campos[1] == password)
+                        {
+                            string cargo = campos[2];
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Login bem sucedido!");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine($"Bem vindo, {utilizador}! Cargo: {cargo}.");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception ex) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Erro ao verificar dados de login: {ex.Message}");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Text;
 
 namespace LP1_Livraria
 {
@@ -38,12 +36,9 @@ namespace LP1_Livraria
                         Console.Write("Password: ");
                         string password = Console.ReadLine();
 
-                        if (VerificarLogin(utilizador, password))
+                        if (Login.VerificarLogin(utilizador, password))
                         {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine("Login bem sucedido!");
-                            Console.ReadKey();
-                            Console.Clear();
+                            
                         }
                         else
                         {
@@ -73,49 +68,6 @@ namespace LP1_Livraria
                     Console.ReadKey();
                     Console.Clear();
                 }
-            }
-        }
-
-        static bool VerificarLogin(string utilizador, string password)
-        {
-            string caminhoArquivo = "..\\..\\DadosUtilizadores.txt";
-
-            try
-            {
-                if (!File.Exists(caminhoArquivo))
-                {
-                    return false;
-                }
-
-                string[] linhas = File.ReadAllLines(caminhoArquivo);
-
-                foreach (string linha in linhas)
-                {
-                    string[] dados = linha.Split(',');
-                    string[] campos = new string[3];
-
-                    if (dados.Length == campos.Length)
-                    {
-                        Array.Copy(dados, campos, campos.Length);
-
-                        if (campos[0] == utilizador && campos[1] == password) 
-                        {
-                            string cargo = campos[2];
-                            Console.WriteLine($"Bem vindo, {utilizador}! Cargo: {cargo}");
-                            return true;
-                        }
-                    }
-                }
-
-                return false;
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Erro ao verificar dados de login: {ex.Message}");
-                Console.ReadKey();
-                Console.Clear();
-                return false;
             }
         }
     }
